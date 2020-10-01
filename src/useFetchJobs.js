0fbc3b10c1aacd82f4cb19c3dea7c65c5ff379ff
Semert <semert2121@gmail.com>
2020-10-01 15:02:57 +0300

    reducer

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,1 +1,35 @@
-export default function useFetchJobs(params, page) {}
+import { useReducer } from "react";
+
+const ACTIONS = {
+  MAKE_REQUEST: "make-request",
+  GET_DATA: "get-data",
+  ERROR: "error",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.MAKE_REQUEST:
+      return { loading: true, jobs: [] };
+    case ACTIONS.GET_DATA:
+      return { ...state, loading: false, jobs: action.payload.jobs };
+    case ACTIONS.ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+        jobs: [],
+      };
+    default:
+      return state;
+  }
+}
+
+export default function useFetchJobs(params, page) {
+  const { state, dispatch } = useReducer(reducer, { jobs: [], loading: true });
+
+  return {
+    jobs: [],
+    loading: false,
+    error: false,
+  };
+}
