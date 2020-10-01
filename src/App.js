69fbeda8c45b9bc3ea2b1b989afef68ce0ca088c
Semert <semert2121@gmail.com>
2020-10-01 14:53:54 +0300

    jobs, loading ,error

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import UseFetchJobs from "./useFetchJobs";
+import { Contanier } from "react-bootstrap";
 
-function App() {
+const App = () => {
+  const { jobs, loading, error } = UseFetchJobs();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contanier>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error, Try Refreshing.</h1>}
+      <h1> {jobs.length} </h1>
+    </Contanier>
   );
-}
+};
 
 export default App;
